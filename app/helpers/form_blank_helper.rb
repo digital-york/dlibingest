0bf39de236e367a98765754899c5dd2c41fe8226
Julie Allinson <j.allinson@gmail.com>
2016-11-20 18:25:27 +0000

    comment on form blank helper

--- app/helpers/form_blank_helper.rb
+++ app/helpers/form_blank_helper.rb
@@ -1,5 +1,7 @@
 module FormBlankHelper
   def show_blank(curation_concern)
+    # fill the field with the first item for a new record
+    # show a blank field on edit
     if curation_concern.new_record?
       false
     else
