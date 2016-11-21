a3781fd6ccd6fb09f3c8a8c63db6042cf430c2de
Julie Allinson <j.allinson@gmail.com>
2016-11-21 18:16:23 +0000

    adding peri's work on show pages; refactored the presenter to be in line with this guide https://github.com/projecthydra-labs/hydra-vagrant/wiki/Dive-Into-Curation-Concerns, ie. don't copy the whole work_show_presenter, just add the fields needed

--- app/presenters/curation_concerns/thesis_show_presenter.rb
+++ app/presenters/curation_concerns/thesis_show_presenter.rb
@@ -1,6 +1,8 @@
 module CurationConcerns
   class ThesisShowPresenter < CurationConcerns::WorkShowPresenter
-    # Metadata Methods
+
+    # Additional Metadata Methods
+    # title, creator, rights, language are already included
     delegate :abstract, :advisor, :keyword, :qualification_level, :awarding_institution, :department,
              :qualification_name, :date_of_award, :former_id, :rights_holder,
              to: :solr_document
