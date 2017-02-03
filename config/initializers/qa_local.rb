dd2e70f090e26a2ce6fe4f8a49e4a31933832cc2
Frank <frank.feng@york.ac.uk>
2017-02-03 14:54:40 +0000

    added JournalService manually as the generator has been run before

--- config/initializers/qa_local.rb
+++ config/initializers/qa_local.rb
@@ -4,3 +4,4 @@ Qa::Authorities::Local.register_subauthority('current_organisations', 'Dlibhydra
 Qa::Authorities::Local.register_subauthority('current_persons', 'Dlibhydra::Terms::CurrentPersonTerms')
 Qa::Authorities::Local.register_subauthority('qualification_names', 'Dlibhydra::Terms::QualificationNameTerms')
 Qa::Authorities::Local.register_subauthority('departments', 'Dlibhydra::Terms::DepartmentTerms')
+Qa::Authorities::Local.register_subauthority('journals', 'Dlibhydra::Terms::JournalTerms')
