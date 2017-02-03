487eb336d1231df49dafb1eb50cdb9eacdeb029e
Frank <frank.feng@york.ac.uk>
2017-02-03 14:47:05 +0000

    generated JournalArticle

---
+++ app/actors/curation_concerns/actors/journal_article_actor.rb
@@ -0,0 +1,8 @@
+# Generated via
+#  `rails generate curation_concerns:work JournalArticle`
+module CurationConcerns
+  module Actors
+    class JournalArticleActor < CurationConcerns::Actors::BaseActor
+    end
+  end
+end
---
+++ app/models/journal_article.rb
@@ -0,0 +1,20 @@
+# Generated via
+#  `rails generate curation_concerns:work JournalArticle`
+class JournalArticle < Dlibhydra::JournalArticle
+
+  include ::CurationConcerns::WorkBehavior
+  # include ::CurationConcerns::BasicMetadata
+  # Change this to restrict which works can be added as a child.
+  # self.valid_child_concerns = []
+  self.human_readable_type = 'Journal Article'
+  validates :title, presence: { message: 'Your journal article must have a title.' }
+
+  class JournalArticleIndexer < CurationConcerns::WorkIndexer
+    include Dlibhydra::IndexesJournalArticle
+  end
+
+  def self.indexer
+    JournalArticleIndexer
+  end
+
+end
