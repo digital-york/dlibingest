46fb6f411e9d4cd58b7545bb894159e8475e7643
Peri Stracchino <ps552@york.ac.uk>
2017-03-06 17:28:05 +0000

    add files to support journal_article, note these do not as yet extend Dlibhydra::JournalArticle

---
+++ app/forms/curation_concerns/journal_article_form.rb
@@ -0,0 +1,7 @@
+# Generated via
+#  `rails generate curation_concerns:work JournalArticle`
+module CurationConcerns
+  class JournalArticleForm < CurationConcerns::Forms::WorkForm
+    self.model_class = ::JournalArticle
+  end
+end
