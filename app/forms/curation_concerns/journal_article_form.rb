d857c55779c7930e902b9f6917f307b2a4470452
Frank <frank.feng@york.ac.uk>
2017-03-29 11:36:13 +0100

    defined required fields for journal article

--- app/forms/curation_concerns/journal_article_form.rb
+++ app/forms/curation_concerns/journal_article_form.rb
@@ -3,5 +3,7 @@
 module CurationConcerns
   class JournalArticleForm < CurationConcerns::Forms::WorkForm
     self.model_class = ::JournalArticle
+
+    self.required_fields = [:title, :creator_string, :journal_resource_ids]
   end
 end
