9cfbf2fb437ae9732d74114727c80d803ba5d566
YODL <infodir-digital@york.ac.uk>
2017-09-06 10:24:08 +0100

    tidy up batch edit tasks, more reliable versions now added

--- app/models/batch_edit.rb
+++ app/models/batch_edit.rb
@@ -12,20 +12,32 @@ include ::CurationConcerns
 include ::Hydra
 
 
-# add  label to existing THESIS_MAIN filesets   TESTED ON MEGASTACK
-#rake batch_edit_tasks:edit_fileset_labels
-def edit_fileset_labels
-	filesets = Object::FileSet.all
-	filesets.each do |fs| 
-	title = fs.title[0]
-	#if label is empty and title = THESIS_MAIN
-	#test label first to avoid wasting time on the big archaeology set
-		if fs.label == nil
-			if title == "THESIS_MAIN"
-				fs.label = title
-				fs.save!
+
+
+# add  label to  THESIS_MAIN filesets for a specified range of theses
+#using this in preference as should exclude unwanted 'orphan' filesets created as tests etc
+#rake batch_edit_tasks:edit_thesis_main_labels[/home/dlib/testfiles/id_listtest.txt]
+def add_thesis_main_labels(id_list_path)
+#work_ids = CSV.parse(id_list_file)
+work_ids = IO.readlines(id_list_path)  
+	work_ids.each do |i|
+	i = i.strip #trailing white space, line ends etc will cause a faulty uri error	
+		t = Object::Thesis.find(i)  
+		puts "got thesis for " + i
+		thesis_title = t.title[0]
+		puts "thesis_title was " + thesis_title
+		members = t.members
+			members.each do |m|
+				id = m.id
+				fs = Object::FileSet.find(id)
+				title = fs.title[0]
+				if title.start_with?('THESIS_MAIN') 
+					if fs.label != "THESIS_MAIN"
+						fs.label = "THESIS_MAIN"
+						fs.save!
+					end
+				end
 			end
-		end
 	end
 end
 
@@ -55,12 +67,15 @@ work_ids = IO.readlines(id_list_path)
 end
 
 # add  label to individual fileset (could also be used to change an existing one)
-#rake batch_edit_tasks:edit_fileset_labels
+#rake batch_edit_tasks:edit_single_fileset_label[id,label]
 def edit_single_fileset_label(fsid, labelname)
 	fs = Object::FileSet.find(fsid)
 				fs.label = labelname
 				fs.save!
 end
+
+
+
 
 
 end # end of class
--- lib/tasks/batch_edit_tasks.rake
+++ lib/tasks/batch_edit_tasks.rake
@@ -10,11 +10,7 @@ task :greet do
 	puts "doing the greet task"
 end
 
-#add a new label to all the THESIS_MAIN filesets
-task :edit_fileset_labels => :environment do
-	b = BatchEdit.new
-	b.edit_fileset_labels
-end
+
 
 #add (or edit) a  label for a single specified fileset
 task :edit_single_fileset_label, [:fsid,:newlabel] => :environment  do|t, args|
@@ -23,13 +19,21 @@ task :edit_single_fileset_label, [:fsid,:newlabel] => :environment  do|t, args|
 end
 
 
-#add a new label to all the THESIS_ADDITIONAL filesets for a specified range of records (hard code into batch_edit.rb :-( )
+#add a new label to all the THESIS_ADDITIONAL filesets for a specified range of records 
 #task :edit_thesis_additional_labels => :environment do
 task :edit_thesis_additional_labels, [:id_list_path] => :environment do|t, args|
 	b = BatchEdit.new
 	b.edit_thesis_additional_labels(args[:id_list_path])
 end
 
+
+
+#ok
+#try this one instead, which get the theses first and then gets their members
+task :edit_thesis_main_labels, [:id_list_path] => :environment do|t, args|
+b = BatchEdit.new
+	b.add_thesis_main_labels(args[:id_list_path])
+end
 
 
 
