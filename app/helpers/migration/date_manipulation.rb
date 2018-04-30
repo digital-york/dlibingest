c9792e9dd3a2529b014db02105a6068e1fc61fe2
straccers <peri.stracchino@york.ac.uk>
2018-04-30 09:21:47 +0100

    added code to normalise dates for exams and student papers according to agreed format

--- app/helpers/migration/date_manipulation.rb
+++ app/helpers/migration/date_manipulation.rb
@@ -7,8 +7,28 @@ include ::CurationConcerns
 include ::Hydra
 
 def say_hi
-puts "hi"
+puts "hi from Datemanipulation"
 end
+
+#read in list of dates from text file in various formats, output list of same dates in normalised form
+#example call 
+#rake metadata_checking_tasks:check_date_normalisation[/home/dlib/migration/lists/input/allDatesFrom_ThesisAndExamPaperModels.txt,/home/dlib/migration/lists/normalised_dates_exams_and_theses.txt]]
+def run_normalisation_check(input_file_path, output_file_path)
+	puts "running the normalisation of all dc:dates listed in " + input_file_path + " outputting to " + output_file_path
+	outputfile = File.open( output_file_path, "a")
+	inputfile = File.readlines(input_file_path).each do |line|
+		date = line.strip
+		newdate = normalise_date(date)
+		if (newdate.length == 4)
+			outputfile.puts(newdate) 
+		else 
+		    outputfile.puts(newdate) + ": " + " from " + date
+		end
+		
+	end	
+	puts "done this"
+	outputfile.close
+end #end run_normalisation_check
 
 #method to ensure date is formatted as a single year yyyy
 def normalise_date(unnormalised_date)
@@ -20,55 +40,30 @@ def normalise_date(unnormalised_date)
 #dd-mm-yyyy
 #yyyy-mm-dd */
 normalised = ""
- if  /^[0-9]{4}\Z/.match  #already in correct form.  
+ if  /^[0-9]{4}\Z/.match(unnormalised_date)  #already in correct form.  
 	normalised = unnormalised_date
- elsif /^[0-9]{4}-[0-9]{2}\Z/.match #yyyy-mm
-	normalised = unnormalised_date[0-4]
- elsif /^[0-9]{2}-[0-9]{4}\Z/.match #mm-yyyy
-	normalised = unnormalised_date[3-8]
- elsif /^[0-9]{4}-[0-9]{4}\Z/.match  #yyyy-yyyy 
-	normalised = unnormalised_date[5-8]	
- elsif 	/^[0-9]{4}-[0-9]{2}-[0-9]{2}\Z/ #yyyy-mm-dd  (actual order of month/day unimportant)
-    normalised = unnormalised_date[0-4]
- elsif 	/^[0-9]{2}-[0-9]{2}-[0-9]{4}\Z/ #mm-dd-yyyy  (actual order of month/day unimportant)	
-	normalised = unnormalised_date[6-9]
+ elsif /^[0-9]{4}[-\s][0-9]{2}\Z/.match(unnormalised_date) #yyyy-mm
+	normalised = unnormalised_date[0..3]
+ elsif /^[0-9]{2}[-\s][0-9]{4}\Z/.match(unnormalised_date) #mm-yyyy
+	normalised = unnormalised_date[3..8]
+ elsif /^[0-9]{4}[-\s][0-9]{4}\Z/.match(unnormalised_date)  #yyyy-yyyy 
+	normalised = unnormalised_date[5..8]
+ elsif 	/^[0-9]{4}[-\s][0-9]{2}[-\s][0-9]{2}\Z/.match(unnormalised_date) #yyyy-mm-dd  (actual order of month/day unimportant)	
+    normalised = unnormalised_date[0..3]
+ elsif 	/^[0-9]{2}[-\s][0-9]{2}[-\s][0-9]{4}\Z/.match(unnormalised_date) #mm-dd-yyyy  (actual order of month/day unimportant)	
+	normalised = unnormalised_date[6..9] 
  else
      #return whatever we've found - its better than nothing
 	 normalised = unnormalised_date
+	 puts "found variant unknown"
+ end 
+ if (normalised.length != 4)
+ puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" + unnormalised_date +"!!!!!!!!!!!!!!!!!!!!!!!!!"
  end
     return normalised
 end
 
-listfile = File.open( "/home/dlib/lists/uniqueDSlists/"+ outputfilename, "a")
-Dir.foreach(foxmlpath)do |item|
-# we dont want to try and act on the current and parent directories
-next if item == '.' or item == '..'
-dsmap = []
-    path = foxmlpath +"/" + item.to_s
-	puts "working on " + item.to_s
-	doc = File.open(path){ |f| Nokogiri::XML(f, Encoding::UTF_8.to_s)}
-    ns = doc.collect_namespaces	
-	ds_ids = nil
-	ds_ids = doc.xpath("/foxml:digitalObject/foxml:datastream[@ID]",ns)  #KALE
-	ds_ids.each { |id| 	
-		idname = id.attr('ID')
-		idname = idname.to_s
-		idstate = id.attr('STATE')
-		if !dsmap.include? idname
-			if idstate.to_s == "A"
-				dsmap.push(idname)
-			end
-		end	
-	}	
-	if !dsmap.include? "EXAM_PAPER"
-				puts "this record had no EXAM_PAPER"				
-				listfile.puts("no active EXAM_PAPER in " + item)
-	end	
-end
-    listfile.close
-	doc = nil
-	puts "all done, written to /home/dlib/lists/uniqueDSlists/" + outputfilename
-end 
+
 
 
 end # end of class
